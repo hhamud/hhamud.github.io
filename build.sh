dd2f37d65ecf898109fce2a52912c4675dff1948
User <53880692+hhamud@users.noreply.github.com>
2024-02-05 22:50:56 +0000

    fix: build script error

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Step 1: Tangle the Org file
-emacs --batch --eval "(require 'org)" --eval "(org-babel-tangle-file \"build.org\")"
+emacs --batch --eval "(require 'org)" --eval "(org-babel-tangle-file \"posts/build.org\")"
 
 # Step 2: Run the tangled Emacs Lisp script
 emacs -Q --script build.el
